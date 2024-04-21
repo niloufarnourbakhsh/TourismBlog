c6d5b27ae49f7e5955bf1cc964d5ad1626c3431a
niloofar nourbakhsh <niloofar.noorbakhsh@yahoo.com>
2024-04-22 01:21:56 +0330

    put signIn mthod in testCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,16 @@
 
 namespace Tests;
 
+use App\Models\Role;
+use App\Models\User;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function signeIn($user=null)
+    {
+      return  $this->actingAs($user ?? User::factory(Role::create(['name' => 'Admin']))->create());
+    }
 }
