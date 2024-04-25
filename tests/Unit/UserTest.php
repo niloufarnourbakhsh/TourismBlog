afcefd53757d76b499c65373340d348fdb48a254
niloofar nourbakhsh <niloofar.noorbakhsh@yahoo.com>
2024-04-25 17:39:38 +0330

    I deleted IsAdmin unit test and I created a UserTest to put all the user test in one class

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,29 +2,33 @@
 
 namespace Tests\Unit;
 
-use App\Http\Middleware\IsAdmin;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Collection;
 use Tests\TestCase;
-
-class IsAdminTest extends TestCase
+class UserTest extends TestCase
 {
     use RefreshDatabase;
 
-    protected $seed = true;
     /** @test */
     public function admin_role_is_sent_to_is_admin()
     {
         $user=User::factory(Role::create(['name'=>'Admin']))->create();
         $this->assertTrue($user->IsAdmin());
     }
-
     /** @test */
     public function User_role_is_sent_to_is_admin()
     {
         $this->withoutExceptionHandling();
         $user=User::factory(Role::create(['name'=>'User']))->create();
         $this->assertFalse($user->IsAdmin());
+    }
+
+    /** @test */
+    public function a_user_can_have_many_project()
+    {
+        $user=User::factory()->create();
+        $this->assertInstanceOf(Collection::class,$user->posts);
     }
 }
