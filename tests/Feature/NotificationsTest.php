9318efe3b029e0b3e671cb3fb9081797c6829032
niloofar nourbakhsh <niloofar.noorbakhsh@yahoo.com>
2024-04-30 18:50:04 +0330

    some minor changes

--- tests/Feature/NotificationsTest.php
+++ tests/Feature/NotificationsTest.php
@@ -17,22 +17,22 @@ class NotificationsTest extends TestCase
     /** @test */
     public function admin_can_see_all_the_notifications()
     {
-        $post=Post::factory()->create(['user_id'=>$this->userSigneIN()->id]);
-        $comment=$post->user->comments()->create(['body'=>'hii','post_id'=>$post->id]);
+        $post=Post::factory()->create(['user_id'=>$this->signeIn()->id]);
+        $comment=($user=$this->userSigneIN())->comments()->create(['body'=>'hii','post_id'=>$post->id]);
         Auth::logout();
-        $notification=new CommentNotification($post->user,$comment);
+        $notification=new CommentNotification($user,$comment);
         $this->signeIn()->notify($notification);
         $this->get('notifications')->assertStatus(200)
-        ->assertSeeText($post->user->name);
+        ->assertSeeText($user->name);
     }
     /** @test */
     public function admin_can_mark_a_notifications_as_read()
     {
-        $post=Post::factory()->create(['user_id'=>$this->userSigneIN()->id]);
-        $comment=$post->user->comments()->create(['body'=>'hii','post_id'=>$post->id]);
+        $post=Post::factory()->create(['user_id'=>$this->signeIn()->id]);
+        $comment=($user=$this->userSigneIN())->comments()->create(['body'=>'hii','post_id'=>$post->id]);
         Auth::logout();
         $admin=$this->signeIn();
-        $admin->notify(new CommentNotification($post->user,$comment));
+        $admin->notify(new CommentNotification($user,$comment));
         $notifications =$admin->notifications;
         $notification=$notifications->first();
         $this->get('/notification/markNotification/'.$notification->id);
